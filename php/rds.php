07b6a1203bc5ca3b382b906d19385f6542f0415c
Andrew Katz <aakatz3@gmail.com>
2016-10-25 22:51:59 -0400

    disable pdo

--- php/rds.php
+++ php/rds.php
@@ -3,7 +3,7 @@ $dbhost = $_SERVER['RDS_HOSTNAME'];
 $dbport = $_SERVER['RDS_PORT'];
 $dbname = $_SERVER['RDS_DB_NAME'];
 
-$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
+#$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
 $username = $_SERVER['RDS_USERNAME'];
 $password = $_SERVER['RDS_PASSWORD'];
 
