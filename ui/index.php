5884a540f6a1e6a178c6b7105c9af5cf81aac1fc
Andrew Katz <aakatz3@gmail.com>
2016-11-05 11:05:06 -0400

    Change table name

--- ui/index.php
+++ ui/index.php
@@ -6,7 +6,7 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT * FROM alumnitable";
+    $sql = "SELECT * FROM alum_info";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
