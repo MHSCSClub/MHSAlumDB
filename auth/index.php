c1aaea186b27334520b58084f14359994f06454f
Andrew Katz <aakatz3@gmail.com>
2016-11-08 11:26:54 -0500

    Update auth php
    fix infinate redirect
    
    .
    
    .

--- auth/index.php
+++ auth/index.php
@@ -1,9 +1,20 @@
 <?php
     //TODO: If logged in, redir to ui, else redir to login
+    include( '../php/rds.php' );
+    include("../php/signal.class.php");
+    include("../php/auth.php");
+    session_start();
     if(!isset($_COOKIE['alumdbauth'])){
-        header("location: login/");
+        header("location: /auth/login");
         exit;
     } else {
-        echo $_COOKIE['alumdbauth'];
+        $resp = auth::check_auth($_COOKIE['alumdbauth']);
+        if($resp->isError()){
+            header("location: /auth/login");
+            exit;
+        } else {
+            //echo $_COOKIE['alumdbauth'];
+            header("location: /ui/");
+        }
     }
 ?>
