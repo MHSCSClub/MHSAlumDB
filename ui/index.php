9c3dfb7c7f1497ce10cfd5d29a2fd05c54b7a063
Andrew Katz <aakatz3@gmail.com>
2016-11-16 08:42:47 -0500

    Edit Jordan's code, to make it work and fix spelling errors, add limits so browsers don't cry, fix db, and reformat. Also fix EB

--- ui/index.php
+++ ui/index.php
@@ -16,21 +16,43 @@
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-            $sql = "SELECT * FROM alum_info";
-            $result = $conn->query($sql);
+            $page = 1;
+            if(isset($_GET["pageNum"])){
+                $page = filter_input(INPUT_GET, 'pageNum', FILTER_VALIDATE_INT);
+            }
+            
+            $start = $page * 1000 - 1000;
 
-             
+            if($start < 1){
+                $start = 1;
+            }
+
+
+            $query = "SELECT firstName,lastName,graduationYear FROM `alum_info`";
+            if(isset($_GET['search']))
+            {
+                echo $_GET['search'];
+                    $valueToSearch = "%" . mysqli_real_escape_string($conn, $_GET['search']) . "%";
+                    // search in all table columns
+                    // using concat mysql function
+                    $query = $query . " WHERE (lastName LIKE '{$valueToSearch}') OR (firstName LIKE '{$valueToSearch}') OR (graduationYear LIKE '{$valueToSearch}')";   
+            }
+
+            $query = $query .  " ORDER BY graduationYear LIMIT {$start}, 1000";
+            echo $query;
+            $result = $conn->query($query);
+            $num_rows = $result->num_rows;           
 
     
             $tablecode = "";
             if ($result->num_rows > 0) {
                 // output data of each row
-                $tablecode = "<table style=\"width:100%\" border=\"1\"><tr><th>Firstname</th><th>Lastname</th><th>Graduation Year</th></tr>";
+                $tablecode = "<table class=\"table\" id=\"table\" style=\"width:100%\" border=\"1\"><thread><tr><th>Firstname</th><th>Lastname</th><th>Graduation Year</th></tr></thread><tbody>";
                 while($row = $result->fetch_assoc()) {
                     
                     $tablecode = $tablecode . "<tr><td>" . $row["firstName"]. "</td><td>" . $row["lastName"]. "</td><td>" . $row["graduationYear"]. "</td></tr>";
                 }
-                $tablecode . "</table>";
+                $tablecode = $tablecode . "</tbody></table><br>{$num_rows} Results";
                         //replace error placeholder
                 
             } else {
