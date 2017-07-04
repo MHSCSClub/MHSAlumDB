8df2eaa5402eaef24388813323b97390c09b84f6
mb10456 <maxdbobby@gmail.com>
2017-07-04 14:58:08 -0400

    changed
    log

--- auth/logout/index.php
+++ auth/logout/index.php
@@ -3,6 +3,7 @@
         include("../../php/signal.class.php");
         include("../../php/auth.php");
         session_start();
+        
 ?>
 <html>
     <head>
@@ -10,23 +11,24 @@
     </head>
     <body>
         <?php
-            if(!isset($_COOKIE['alumdbauth'])){
+            /*if(!isset($_COOKIE['alumdbauth'])){
                 die("You have already logged out");
             } else {
                 $resp = auth::logout($_COOKIE['alumdbauth']);
             if($resp->isError()){
                 die("Error logging out");
-            } else {
+            } else {*/
                 $cookie_name = 'alumdbauth';
                 unset($_COOKIE[$cookie_name]);
                 // empty value and expiration one hour before
                 $res = setcookie($cookie_name, null, -1, '/');
+                unset($_SESSION['username'];
+                
+                
                 echo "Logged out successfully";
                 exit;
-            }
+            
         ?>
-
-        
         <p>Redirecting to login</p>
         <script type="text/javascript">
             t1 = window.setTimeout(function(){ window.location = "/auth/login"; },3000);
