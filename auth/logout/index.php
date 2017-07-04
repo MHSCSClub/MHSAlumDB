a8f5bfaf241fda4621d34355cc4a4d1b18b59c43
mb10456 <maxdbobby@gmail.com>
2017-07-04 14:25:14 -0400

    Added line
    exit

--- auth/logout/index.php
+++ auth/logout/index.php
@@ -1,37 +1,45 @@
 <?php
-        include( '../../php/rds.php' );
+       /* include( '../../php/rds.php' );
         include("../../php/signal.class.php");
         include("../../php/auth.php");
-        session_start();
+        session_start();*/
+        // Inialize session
+session_start();
+
+// Delete certain session
+unset($_SESSION['username']);
+// Delete all session variables
+// session_destroy();
+
+// Jump to login page
+header('Location: index.php');
         
 ?>
-<html>
+<!--<html>
     <head>
         <title>Logged out</title>
     </head>
     <body>
         <?php
-            /*if(!isset($_COOKIE['alumdbauth'])){
+            if(!isset($_COOKIE['alumdbauth'])){
                 die("You have already logged out");
             } else {
                 $resp = auth::logout($_COOKIE['alumdbauth']);
             if($resp->isError()){
                 die("Error logging out");
-            } else {*/
+            } else {
                 $cookie_name = 'alumdbauth';
                 unset($_COOKIE[$cookie_name]);
                 // empty value and expiration one hour before
                 $res = setcookie($cookie_name, null, -1, '/');
-                unset($_SESSION['username'];
-                
-                
                 echo "Logged out successfully";
                 
-            
+                exit;
+            }
         ?>
         <p>Redirecting to login</p>
         <script type="text/javascript">
             t1 = window.setTimeout(function(){ window.location = "/auth/login"; },3000);
         </script>
     </body>
-</html>
+</html>-->
