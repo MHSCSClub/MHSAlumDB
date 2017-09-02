746568150f1f8e71fd11cc880a60fa5637810fa8
mb10456 <maxdbobby@gmail.com>
2017-09-02 00:35:54 -0400

    Fix3

--- auth/forgot/index.php
+++ auth/forgot/index.php
@@ -1,21 +1,2 @@
-<?php
-    readfile("forgot.html");
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-    include("../../php/signal.class.php");
-    include("../../php/auth.php");
-    
-    session_start();
-    // email sent from form
-    $email = $_POST["email"];
-    $check_email = mysqli_query($conn, "SELECT Email FROM crud where Email = '$email' ");
-    if(mysqli_num_rows($check_email) > 0){
-        $msg = "First line of text\nSecond line of text";
-        mail($email,"My subject",$msg);
-    
-    }
-    else {
-        readfile("forgot.html");
-    }
-}
-?>
+
 
