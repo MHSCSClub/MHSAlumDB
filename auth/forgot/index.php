64dccd800334584b06a6489e781de2526c108a69
mb10456 <maxdbobby@gmail.com>
2017-09-02 00:30:26 -0400

    passwordResetAttempt

--- auth/forgot/index.php
+++ auth/forgot/index.php
@@ -1,5 +1,20 @@
 <?php
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+    include("../../php/signal.class.php");
+    include("../../php/auth.php");
+    
     session_start();
-    readfile("forgot.html");
+    // email sent from form
+    $email = $_POST["email"];
+    $check_email = mysqli_query($conn, "SELECT Email FROM crud where Email = '$email' ");
+    if(mysqli_num_rows($check_email) > 0){
+        $msg = "First line of text\nSecond line of text";
+        mail($email,"My subject",$msg);
+    
+    }
+    else {
+        readfile("forgot.html");
+    }
+}
 ?>
 
---
+++ auth/forgot/send.php
@@ -0,0 +1,11 @@
+<?php
+  $resp = auth::login($_POST['username'])
+  if($resp->isError()){
+      
+  }
+
+
+
+
+
+?>
