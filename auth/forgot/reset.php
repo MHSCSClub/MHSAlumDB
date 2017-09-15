ffc7a18be9a78098a340cc7929a8b4864b57e83f
mb10456 <maxdbobby@gmail.com>
2017-09-14 20:37:43 -0400

    structure changes

--- auth/forgot/reset.php
+++ auth/forgot/reset.php
@@ -27,31 +27,7 @@ if (isset($_POST["ForgotPassword"])) {
 		exit;
 	}
 
-	// Check to see if a user exists with this e-mail
-	$stmt = $conn->prepare('SELECT email FROM users WHERE email = :email');
-	$stmt->bind_param(':emails', $email);
-	$stmt->execute();
-	$userExists = $stmt->fetch(PDO::FETCH_ASSOC);
-	$conn = null;
 	
-	if ($userExists["email"])
-	{
-		// Create a unique salt. This will never leave PHP unencrypted.
-		$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
-
-		// Create the unique user password reset key
-		$password = hash('sha512', $salt.$userExists["email"]);
-
-		// Create a url which we will direct them to reset their password
-		$pwrurl = "www.yoursitehere.com/reset_password.php?q=".$password;
-		
-		// Mail them their key
-		$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website www.yoursitehere.com\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
-		sendmail($userExists["email"], "www.yoursitehere.com - Password Reset", $mailbody);
-		echo "Your password recovery key has been sent to your e-mail address.";
-		
-	}
-	else
-		echo "No user with that e-mail address exists.";
 }
+sendmail_reset($email);
 ?>
