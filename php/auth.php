7b2c7ce8c1102ce8bc81e3a6bbd56b1456221f08
mb10456 <maxdbobby@gmail.com>
2017-09-14 20:58:09 -0400

    undo metjod

--- php/auth.php
+++ php/auth.php
@@ -5,32 +5,6 @@
 
     class auth
     {
-		private static function sendmail_reset($email)
-		{
-			$db = self::getConnection();
-			// Check to see if a user exists with this e-mail
-			$stmt = $db->prepare('SELECT email FROM users WHERE email = :email');
-			$stmt->bind_param(':emails', $email);
-			$stmt->execute();
-			$res = $stmt->get_result();
-	
-			if($res->num_rows > 0)
-			{
-			// Create a unique salt. This will never leave PHP unencrypted.
-			$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
-
-			// Create the unique user password reset key
-			$password = hash('sha512', $salt.$userExists["email"]);
-
-			// Create a url which we will direct them to reset their password
-			$pwrurl = "www.yoursitehere.com/reset_password.php?q=".$password;
-		
-			// Mail them their key
-			$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website www.yoursitehere.com\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
-			sendmail($userExists["email"], "www.yoursitehere.com - Password Reset", $mailbody);
-			echo "Your password recovery key has been sent to your e-mail address.";
-			}
-		}
         public static function login($username, $password) { 
 			return self::run(function() use ($username, $password) {
 				return auth::REAL_login($username, $password);
@@ -267,12 +241,6 @@
 
 			return Signal::success();
 		}
-		
-		
-	}
-	else
-		echo "No user with that e-mail address exists.";
-		}
         private static function sendmail_register($username, $key)
         {
 			$base_url =  "https://{$_SERVER['HTTP_HOST']}/auth/register";
@@ -385,8 +353,7 @@
             } else {
                 echo("Email sent!"."\n");
             }
-		}
-		
+        }
         
     }
 ?>
