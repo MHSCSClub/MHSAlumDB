36b0ebfc0cd1f24b87fa2d43fc38515a28935646
mb10456 <maxdbobby@gmail.com>
2017-10-25 20:08:15 -0400

    reset_password

--- auth/forgot/reset.php
+++ auth/forgot/reset.php
@@ -1,10 +1,7 @@
 <?php
 ini_set('display_errors', 1);
-// Connect to MySQL
-    $username = "username"; 
-    $password = "password"; 
-    $host = "localhost"; 
-    $dbname = "databasename"; 
+include("../../php/auth.php");
+	
 
 	// Was the form submitted?
 	if (isset($_POST["ForgotPassword"])) {
@@ -20,23 +17,29 @@ ini_set('display_errors', 1);
 		exit;
 		}
 	
-	$db = getConnection();
+	$db = auth::getConnection();
 	// Check to see if a user exists with this e-mail
 	$query = $db->prepare('SELECT email FROM users WHERE email = :email');
 	$query->bindParam(':email', $email);
 	$query->execute();
 	$res = $stmt->get_result();
 
-	if ($res->num_rows > 0)
+	if ($res->num_rows == 1)
 	{
-	// Create a unique salt. This will never leave PHP unencrypted.
-	$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
+		// Create a unique salt. This will never leave PHP unencrypted.
+		$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
 
 	// Create the unique user password reset key
-	$password = hash('sha512', $salt.$userExists["email"]);
+	$password_hash = hash('sha512', $salt.$userExists["email"]);
 
 	// Create a url which we will direct them to reset their password
-	$pwrurl = "www.yoursitehere.com/reset_password.php?q=".$password;
+	$pwrurl = "www.yoursitehere.com/reset_password.php?q=".$password_hash;
+	
+	$squery = $db->prepare('UPDATE users SET password_reset_hash = :password_reset_hash, reset_expiration_timestamp = NOW(), is_password_reset_active = 1');
+	$squery->bindParam(':password_reset_hash', $password_hash);
+	$squery->execute();
+
+
 
 	// Mail them their key
 	$mailbody = "Dear user,\n\nIf this e-mail does not apply to you please ignore it. It appears that you have requested a password reset at our website www.yoursitehere.com\n\nTo reset your password, please click the link below. If you cannot click it, please paste it into your web browser's address bar.\n\n" . $pwrurl . "\n\nThanks,\nThe Administration";
