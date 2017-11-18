082b7d72ef612cb0e18eb85f37774c3b369e49f5
mb10456 <maxdbobby@gmail.com>
2017-11-17 19:14:04 -0500

    tries

--- auth/forgot/reset.php
+++ auth/forgot/reset.php
@@ -36,9 +36,10 @@ include("../../php/auth.php");
 		if ($res->num_rows == 1)
 		{
 			// Create a unique salt. This will never leave PHP unencrypted.
-			$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
+			//$salt = "498#2D83B631%3800EBD!801600D*7E3CC13";
 			// Create the unique user password reset key
-			$password_hash = hash('sha512', $salt.$email);
+			//$password_hash = hash('sha512', $salt.$email);
+			$password_hash = hash('sha512', $email);
 			// Create a url which we will direct them to reset their password
 			$pwrurl = "https://alumdb.mamaroneckschoolsfoundation.org/auth/reset_password.php?q=".$password_hash;
 			
