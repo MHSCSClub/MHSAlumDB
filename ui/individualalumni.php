94e2ea414441c9f7d599099005dac054f6d82afc
mb10456 <maxdbobby@gmail.com>
2017-11-22 19:18:22 -0500

    link to alumni info

---
+++ ui/individualalumni.php
@@ -0,0 +1,20 @@
+<?php
+    $conn = new mysqli($dbhost, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $alumnitable_id = $_POST["alumniid"];
+    $query = "SELECT state,country FROM `alum_info` WHERE alumnitable_id = " . $alumnitable_id;
+    $result = $conn->query($query); 
+    $num_rows = $result->num_rows;
+    if ($num_rows == 1) { 
+        // output data of each row
+        echo(row["state"]);
+        echo(row["country"]);
+    }
+    else{
+        trigger_error("error");
+    }
+
+?>
