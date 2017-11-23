cebbbd42d4beb6f515c46dc019240e72ccc216e0
mb10456 <maxdbobby@gmail.com>
2017-11-22 21:05:37 -0500

    form simplified

--- auth/reset/reset_password_form.php
+++ auth/reset/reset_password_form.php
@@ -1,4 +1,4 @@
-<?php /*echo '
+<?php echo '
 <form action="reset.php" method="POST">
 E-mail Address: <input type="text" name="email" size="20" /><br />
 New Password: <input type="password" name="password" size="20" /><br />
@@ -9,37 +9,4 @@ if (isset($_GET["q"])) {
 }
 	echo '" /><input type="submit" name="ResetPasswordForm" value=" Reset Password " />
 </form>';
-*/
 ?>
-
-<form action="reset.php" method="POST">
-<head>
-	<meta charset="UTF-8">
-	<title>MHS Alum DB - Forgot Password</title>
-	<link rel="stylesheet" href="/css/jquery-ui.css">
-	<link rel="stylesheet" href="/css/style.css" media="screen" type="text/css" />
-	<link rel="shortcut icon" href="/favicon.ico" />
-	<!-- A Pen created at CodePen.io. You can find this one at http://codepen.io/Mongeed/pen/IuBLt. -->
-	<!-- A simple log-in screen I pretty much made out of boredom. Style is based on google's login which is one of my favorite login's. Tried to keep it minimalistic, I think it worked. -->
-</head>
-    <body>
-    
-    <div class="card">
-        <img src="/img/msf.png"><br>
-        E-mail Address: <input type="text" name="email" size="20" /> 
-		New Password: <input type="password" name="password" size="20" /> 
-		Confirm Password: <input type="password" name="confirmpassword" size="20" /> 
-		<input type="submit" name="ResetPasswordForm" value="Reset Password" />
-		
-		<input type="hidden" name="q" value="";
-			if (isset($_GET["q"])) {
-				echo $_GET["q"];
-			}		
-
-		
-        <div class="help">
-			<a href="../login/">Log In</a> • <a href="../register/">Register</a>
-		</div>
-    </div>
-    <script src="/js/jquery_and_jqueryui.js"></script>
-</form>
