1b2ea5d66400615a46d03b433c619e41fc9c7347
mb10456 <maxdbobby@gmail.com>
2018-05-22 19:13:25 -0400

    update users

--- pairIDexisting/index.php
+++ pairIDexisting/index.php
@@ -7,8 +7,8 @@
     }
     $alumnitable_id = $_GET["alumniid"];
     $indivUser = $_SESSION['individual'];
-    $conn->query("UPDATE 'users' SET userid = $alumnitable_id WHERE username =$indivUser");
-    $conn->query("UPDATE 'users' SET firstLogin = 0 WHERE username =$indivUser");
+    $conn->query("UPDATE 'users' SET userid = $alumnitable_id WHERE username ='$indivUser'");
+    $conn->query("UPDATE 'users' SET firstLogin = 0 WHERE username ='$indivUser'");
     
     
 ?>
