55ab919e7589b1621494da7f159f9a6357c4ed6f
Pablo Garza <pgarzabo2@hotmail.com>
2018-05-22 12:04:33 -0400

    Comment

--- auth/login/index.php
+++ auth/login/index.php
@@ -2,19 +2,19 @@
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       include("../../php/signal.class.php");
       include("../../php/auth.php");
-      
+      // Max Bobby
       session_start();
-      // username and password sent from form 
+      // username and password sent from form
       $resp = auth::login($_POST['username'], $_POST['password']);
       $_SESSION['individual'] = $_POST['username'];
       //print(json_encode($resp->toArray()));
       //print(json_encode($resp->getData()));
       if($resp->isError()){
         $error = $resp->getMessage();
-        //replace error placeholder 
+        //replace error placeholder
         $e_string = "<div id=\"error\"><img src=\"/img/Delete-icon.png\"/><p>{$error}</p></div>";
         $reghtm = file_get_contents('./login.html', FILE_USE_INCLUDE_PATH);
-        die(str_replace("<!-- ERROR -->", $e_string, $reghtm));                    
+        die(str_replace("<!-- ERROR -->", $e_string, $reghtm));
       } else {
         setcookie("alumdbauth", $resp->getData()["authcode"],$resp->getData()["expire"], "/", $_SERVER['SERVER_NAME'], true);
         header("location: /ui/");
