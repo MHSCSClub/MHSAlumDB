f0b74db833acb2cc3fd1c53cfa649c5567c19f9a
mb10456 <maxdbobby@gmail.com>
2018-05-22 18:36:39 -0400

    pairing ids

---
+++ pairIDexisting/index.php
@@ -0,0 +1,32 @@
+<?php
+    echo "Pairing account to previously entered info";
+    include( '../../php/rds.php' );
+    include("../../php/signal.class.php");
+    include("../../php/auth.php");
+    
+    $conn = new mysqli($dbhost, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $alumnitable_id = $_GET["alumniid"];
+    $indivUser = $_SESSION['individual'];
+    $query = "UPDATE users SET userid = $alumnitable_id WHERE username = " . $indivUser;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
+?>
+
+<html>
+    <head>
+        <title>Thank you for setting up your acccount</title>
+    </head>
+    <body>
+        <p>Redirecting to UI...Welcome to the MHS Alumni Database</p>
+        <script type="text/javascript">
+            t1 = window.setTimeout(function(){ window.location = "/auth/login"; },3000);
+        </script>
+    </body>
+</html>
