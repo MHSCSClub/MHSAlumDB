70ac64467209d1605ebcd9543cfa8b3a894fe95f
mb10456 <maxdbobby@gmail.com>
2018-05-23 10:47:54 -0400

    incorrect

--- mainprofile/index.php
+++ mainprofile/index.php
@@ -112,7 +112,7 @@
             // assign info in array to variables
             $row = $result->fetch_assoc();
             $firstname= $row["firstName"];
-            $lastname= $row["lastname"];
+            $lastname= $row["lastName"];
             $state= $row["state"];
             $country= $row["country"];
             /*$tablecode = "<table class=\"table\" id=\"table\" style=\"width:100%\" border=\"1\"><thead><tr><th>Firstname</th><th>Lastname</th><th>State</th><th>Country</th></tr></thead><tbody>";
@@ -126,7 +126,7 @@
         echo $lastname;
         echo $state;
         echo $country;
-        
+
         $conn->close();
 
         
