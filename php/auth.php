e75cf0a32ba0c061bb5afb01c8cdde43f9bae66b
Max Bobby <maxdbobby@gmail.com>
2018-05-23 11:59:32 -0400

    changegyeartostring

--- php/auth.php
+++ php/auth.php
@@ -317,7 +317,7 @@
 
 			//Insert user into database
 			$stmt = $db->prepare("INSERT INTO setupusers (username, firstname, lastname, graduationyear, authkey) VALUES (?, ?) ON DUPLICATE KEY UPDATE authkey = ?");
-			$stmt->bind_param('sssiss', $email, $firstname, $lastname, $gyear, $key, $key);
+			$stmt->bind_param('ssssss', $email, $firstname, $lastname, $gyear, $key, $key);
 			$stmt->execute();
 			$stmt->close();
             
