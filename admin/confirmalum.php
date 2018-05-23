348ab9de2d10933d7f006bb6f00229ebd346047b
Max Bobby <maxdbobby@gmail.com>
2018-05-23 13:45:15 -0400

    trying new include statements

--- admin/confirmalum.php
+++ admin/confirmalum.php
@@ -2,7 +2,8 @@
         ini_set('display_errors', 1);
         include("../../php/signal.class.php");
         include("../../php/auth.php");
-        include ('../php/rds.php');
+        include('../../php/rds.php' );
+
         $conn = new mysqli($dbhost, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
