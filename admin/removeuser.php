4009df8a569dd1996560382b8ade1a65819f2f0f
Pablo Garza <pgarzabo2@hotmail.com>
2018-05-24 14:17:58 -0400

    bind_param check

--- admin/removeuser.php
+++ admin/removeuser.php
@@ -4,7 +4,6 @@
   include("../php/signal.class.php");
   include("../php/auth.php");
   include('../php/rds.php');
-
   $conn = new mysqli($dbhost, $username, $password, $dbname);
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
@@ -12,8 +11,9 @@
   $username = $_GET["userName"];
   echo "username : " . $username;
   $stmt = $conn->prepare('IF  EXISTS (SELECT * FROM setupusers WHERE username = ?) DROP USER [?]');
-  $stmt->bind_param('ss', $username, $username);
+  if(!$stmt->bind_param('ss', $username, $username)){
+    echo "bind_param returned false";
+  }
   $stmt->execute();
   echo $username . " removed.";
-
 ?>
