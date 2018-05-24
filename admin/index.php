a4d273b4b73c4305b78e0f89d9ee8816640afe5b
Pablo Garza <pgarzabo2@hotmail.com>
2018-05-24 12:35:58 -0400

    remove user test

--- admin/index.php
+++ admin/index.php
@@ -20,7 +20,7 @@
             header("location: /auth/");
             exit;
         }
-          
+
     }
     include ('../php/rds.php');
     $conn = new mysqli($dbhost, $username, $password, $dbname);
@@ -29,7 +29,7 @@
     }
 
             $query = "SELECT username,firstname,lastname,graduationyear, confirmkey FROM `setupusers`";
-            $result = $conn->query($query);           
+            $result = $conn->query($query);
             $num_rows_full = $result->num_rows;
             $max_pages = ceil($num_rows_full/100);
             $page = 1;
@@ -49,16 +49,16 @@
                 $start = 0;
             }
             $query = $query .  " ORDER BY graduationyear LIMIT {$start}, 100";
-            $result = $conn->query($query);           
+            $result = $conn->query($query);
             $tablecode = "";
             $num_rows = $result->num_rows;
 
 
             if ($num_rows > 0) {
                 // output data of each row
-                $tablecode = "<table class=\"table\" id=\"table\" style=\"width:100%\" border=\"1\"><thead><tr><th>Firstname</th><th>Lastname</th><th>Graduation Year</th></tr></thead><tbody>";
+                $tablecode = "<table class=\"table\" id=\"table\" style=\"width:100%\" border=\"1\"><thead><tr><th>Firstname</th><th>Lastname</th><th>Graduation Year</th><th>Remove User</th></tr></thead><tbody>";
                 while($row = $result->fetch_assoc()) {
-                    $tablecode = $tablecode . '<tr><td><a href="https://alumdb.mamaroneckschoolsfoundation.org/admin/confirmalum.php?userName=' . $row["username"]. '">' . $row["username"]. '</a></td><td>' . $row["firstname"]. "</td><td>" . $row["lastname"]. "</td><td>" . $row["graduationyear"]. "</td></tr>";
+                    $tablecode = $tablecode . '<tr><td><a href="https://alumdb.mamaroneckschoolsfoundation.org/admin/confirmalum.php?userName=' . $row["username"]. '">' . $row["username"]. '</a></td><td>' . $row["firstname"]. "</td><td>" . $row["lastname"]. "</td><td>" . $row["graduationyear"]. "</td><form action='removeuser.php' method='post'><input type='submit' name='remove' value='GO'/></form></tr>";
                 }
                 $tablecode = $tablecode . "</tbody></table>";
             }
@@ -68,9 +68,9 @@
                 $res_str = $res_str . "s";
             }
 
-            $res_str = $res_str . ", {$num_rows} shown (page {$page} of " . $max_pages . ")<br>"; 
+            $res_str = $res_str . ", {$num_rows} shown (page {$page} of " . $max_pages . ")<br>";
             $base_url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) . "?page=";
-                                
+
             $page_array = array();
 
             $pages = array();
@@ -78,12 +78,12 @@
                 $pages["First"]=1;
                 $pages["Previous"]=$page - 1;
             }
-                                
+
             if($page != $max_pages){
                 $pages["Next"]=$page + 1;
                 $pages["Last"]=$max_pages;
             }
-                                 
+
 
             foreach ($pages as $name=>$pagenum) {
                 $tmp_page = $base_url . $pagenum;
@@ -91,19 +91,14 @@
                     $tmp_page = $tmp_page . "&search=" . $search_str;
                 }
                 $page_array[] = "<a href={$tmp_page}>{$name}</a>";
-            } 
-                                
-                                
-                                
+            }
+
+
+
             $res_str = $res_str . implode(" ", $page_array);
             $res_str = $res_str . "</div>";
             echo $res_str;
-             
+
             $conn->close();
     }
 ?>
-    
-    
-
-
-
