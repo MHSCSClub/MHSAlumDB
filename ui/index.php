bd1c89fe66251d2269364d6e3a713614b5150a69
mb10456 <maxdbobby@gmail.com>
2018-05-24 17:12:03 -0400

    return to old format

--- ui/index.php
+++ ui/index.php
@@ -68,43 +68,28 @@
 <body id="page-top" class="index">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="navbar-header">
-            
+            <a class="navbar-brand" href="#">MHS Alumni Database</a>
         </div>
-        <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
             <li class="nav-item">
-            <a class="nav-link" href="/mainprofile">My profile</a>
+                <a class="nav-link" href="/mainprofile">Profile</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Messages</a>
+                <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">News</a>
+                <a class="nav-link" href="#">Link</a>
             </li>    
-                
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
+               
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="/auth/logout">Logout</a>
+                </li> 
+            </ul>
         </div>  
-    </nav> 
-
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">MHS Alumni Database</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Page 1</a></li>
-                    <li><a href="#">Page 2</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </div>
-        </nav> 
+    </nav>
 
     <section>
 			<div class="container">
