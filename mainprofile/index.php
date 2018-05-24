f8dccec79dc73739504b2d1dd846127fb46ff714
mb10456 <maxdbobby@gmail.com>
2018-05-24 14:39:50 -0400

    change bootstrap to local

--- mainprofile/index.php
+++ mainprofile/index.php
@@ -80,15 +80,12 @@
   <title>Homepage</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
   <style>
-  .fakeimg {
+    .fakeimg {
       height: 200px;
       background: #aaa;
-  }
+    }
   </style>
 </head>
 <body>
@@ -99,14 +96,14 @@
 </div>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="">Directory</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="https://alumdb.mamaroneckschoolsfoundation.org/ui/">Directory</a>
+        <a class="nav-link" href="#">Directory</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
@@ -121,7 +118,6 @@
 <div class="container" style="margin-top:30px">
   <div class="row">
     <div class="col-sm-8">
-      <div class="fakeimg">Fake Image</div>
       <hr>
       <h3> About me </h3>  
       <hr>
@@ -133,7 +129,7 @@
       <hr>
       <h3> Contact information </h3>
       <hr>
-      <p>Lorem ipsum dolor sit ame.</p>
+
       <ul class="nav nav-pills flex-column">
         <p><?php echo "Phone number: " . $phonenumber; ?></p>
       </ul>
