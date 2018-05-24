ebcee04dc628017b1c38b0a129c65371b195adf4
mb10456 <maxdbobby@gmail.com>
2018-05-24 16:33:38 -0400

    changes to navbar

--- ui/index.php
+++ ui/index.php
@@ -72,23 +72,21 @@
 <body id="page-top" class="index">
     <div class="jumbotron" style="margin-bottom:0">
         <h1> Mamaroneck Alumni Database</h1>
-        <a class="nav-link" href="/auth/logout">Logout</a>
     </div>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="">Directory</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">Directory</a>
+                <a class="nav-link" href="/mainprofile">Profile</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
+            </li>    
+            <li class="nav-item">
+                <a class="nav-link" href="/auth/logout">Logout</a>
             </li>    
             </ul>
         </div>  
