1c9d6626fbb68d6a2721a7082c46efbe0bc64802
mb10456 <maxdbobby@gmail.com>
2018-05-24 15:10:01 -0400

    return to old

--- ui/index.php
+++ ui/index.php
@@ -74,12 +74,33 @@
 <body id="page-top" class="index">
     <!-- Navigation -->
     <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-    <div class="jumbotron text-center" style="margin-bottom:0">
-        <h1><?php Mamaroneck Alumni Database ?></h1>
-    </div>
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <header>
+                <a class="navbar-brand" href="#page-top">Mamaroneck Alumni Database</a>
+                </header>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
         <!-- /.container-fluid -->
     </nav>
-    <p><a href="/auth/logout">Logout</a></p>
+    <li>
+        <a href="/auth/logout">Logout</a>
+    </li>
     <!-- This is the table -->
     <section>
 			<div class="container">
