cead70b4aebd824325ab2cc15860f72454ddb9d1
mb10456 <maxdbobby@gmail.com>
2018-05-24 17:03:05 -0400

    additiional navbar

--- ui/index.php
+++ ui/index.php
@@ -68,7 +68,7 @@
 <body id="page-top" class="index">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">MHS Alumni Database</a>
+            
         </div>
         <ul class="navbar-nav">
             <li class="nav-item">
@@ -87,7 +87,24 @@
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         </ul>
         </div>  
-    </nav>
+    </nav> 
+
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">MHS Alumni Database</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </nav> 
 
     <section>
 			<div class="container">
