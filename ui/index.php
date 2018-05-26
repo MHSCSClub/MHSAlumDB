3c722fa262606ed9f4cce1ff338725803f3ed199
mb10456 <maxdbobby@gmail.com>
2018-05-26 19:02:34 -0400

    new searchbar

--- ui/index.php
+++ ui/index.php
@@ -104,6 +104,10 @@
                                     <input type="submit" class="form-control button" value="Submit"/>
                                 </span>
                         </form>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
                 <br>
