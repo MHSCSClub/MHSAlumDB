e93fbd74674e8b14e9ba67daba11a30919365078
mb10456 <maxdbobby@gmail.com>
2018-05-29 00:42:23 -0400

    new attempt

---
+++ editprofile/delete.php
@@ -0,0 +1,24 @@
+<?php
+    echo "hello";
+    include( '../php/rds.php' );
+    include("../php/signal.class.php");
+    ini_set('display_errors', 1);
+    session_start();
+    $conn = new mysqli($dbhost, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else{
+        echo "hello";
+    }
+    $indivUser = $_SESSION['individual'];
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        echo "hi";
+    }
+    else{
+        readfile("editInfo.php");
+    }
+?>
+
+
--- editprofile/index.php
+++ editprofile/index.php
@@ -1,24 +1,193 @@
 <?php
-    echo "hello";
-    include( '../php/rds.php' );
-    include("../php/signal.class.php");
-    ini_set('display_errors', 1);
     session_start();
+    ini_set('display_errors', 1);
+    include('../php/rds.php');
+    include("../php/signal.class.php");
+    include("../php/auth.php");
+    if(!isset($_COOKIE['alumdbauth'])){
+        header("location: /auth/");
+        exit;
+    } else {
+        $resp = auth::check_auth($_COOKIE['alumdbauth']);
+        if($resp->isError()){
+            header("location: /auth/");
+            exit;
+        }
+          
+    }
+    
     $conn = new mysqli($dbhost, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    else{
-        echo "hello";
-    }
     $indivUser = $_SESSION['individual'];
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        echo "hi";
-    }
-    else{
-        readfile("editInfo.php");
-    }
+    
+    //echo '<div style="Color::white">"Welcome "'. $indivUser ' </span>';
+    $sql = "SELECT userid FROM users WHERE username = '$indivUser'";
+    $result = $conn->query($sql);
+    $id;
+        if ($result->num_rows > 0) {
+                    // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $id = $row["userid"];
+            }
+        } else {
+            echo "0 results";
+        }
+        
+        $query = "SELECT firstName, lastName, currentstate, country, graduationYear, phoneNumber FROM `alum_info` WHERE alumnitable_id = " . $id;
+        $result = $conn->query($query);
+        $num_rows = $result->num_rows;
+        //var_dump($result);
+        if ($num_rows == 1) {
+            // assign info in array to variables
+            $row = $result->fetch_assoc();
+            $firstname= $row["firstName"];
+            $lastname= $row["lastName"];
+            $state= $row["currentstate"];
+            $country= $row["country"];
+            $gyear = $row["graduationYear"];
+            $phonenumber = $row["phoneNumber"];
+        }
+        else{
+            trigger_error("error");
+        }
+        
+    $conn->close();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Edit Profile</title>
+        <link href="../css/bootstrap.css" rel="stylesheet"></link>
+    </head>
+                <body>
+                    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">MHS Alumni Database</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                            <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/ui">Directory</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>    
+                            
+                            </ul>
+                            <ul class="nav navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/auth/logout">Logout</a>
+                                </li> 
+                            </ul>
+                        </div>  
+                    </nav>
 
+
+    <div class="container" style="margin-top:30px">
+        <div class="row">
+            
+                <form method="post">
+                        <label for="firstname" class="col-sm-2 col-form-label">Firstname</label>
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                            <input class="form-control" id="firstname" name="firstname" value=<?php echo $firstname;?> type="text">
+                            </div>
+                        </div>
+                        <label for="lastname" class="col-sm-2 col-form-label">Lastname</label>
+                        <div class="form-group row">                           
+                            <div class="col-sm-10">
+                            <input class="form-control" id="lastname" name="lastname" value=<?php echo $lastname;?> type="text">
+                            </div>
+                            
+                        </div>
+                        <label for="state" class="col-sm-2 col-form-label">State</label>
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                            <input class="form-control" id="state" name="state" value=<?php echo $state;?> type="text">
+                            </div>
+                           
+                        </div>
+                        <label for="country" class="col-sm-2 col-form-label">Country</label>
+                        <div class="form-group row">                            
+                            <div class="col-sm-10">
+                            <input class="form-control" id="country" name="country" value=<?php echo $country;?> type="text">
+                            </div>
+                            
+                        </div>
+                        <label for="phonenumber" class="col-sm-2 col-form-label">Phone number</label>
+                        <div class="form-group row">                          
+                            <div class="col-sm-10">
+                            <input class="form-control" id="phonenumber" name="phonenumber" value=<?php echo $phonenumber;?> type="text">
+                            </div>
+                            
+                        </div>
+               
+                   
+                        <div class="form-check">
+                        <input class="form-check-input" value="" id="showFirstname" type="checkbox">
+                        <label class="form-check-label" for="showFirstname">
+                            Show firstname
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" value="" id="showLastname" type="checkbox">
+                        <label class="form-check-label" for="showLastname">
+                            Show lastname
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" value="" id="showState" type="checkbox">
+                        <label class="form-check-label" for="showState">
+                            Show my current state
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" value="" id="showCountry" type="checkbox">
+                        <label class="form-check-label" for="showCountry">
+                            Show my country
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" value="" id="showPhonenumber" type="checkbox">
+                        <label class="form-check-label" for="showPhonenumber">
+                            Show my phone number
+                        </label>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Submit changes</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
+
+<?php
+    $conn = new mysqli($dbhost, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $state = $_POST['state'];
+        $country = $_POST['country'];
+        $phonenumber = $_POST['phonenumber'];
+        $stmt = $conn->query("UPDATE alumni SET firstname = ?, lastname = ?, currentstate = ?, country = ?, phoneNumber = ? WHERE username = ?");
+        $stmt->bind_param('ssssss', $firstname, $lastname, $state, $country, $phonenumber, $indivUser);
+        $stmt->execute();
+        echo "success";
+    }
+    else{
+        echo "error";
+    }
+    
+?>
