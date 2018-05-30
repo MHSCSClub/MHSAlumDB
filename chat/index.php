4ed58dd719afaca8c7e9a4b680554be8c476beb6
Pablo Garza <pgarzabo2@hotmail.com>
2018-05-30 02:07:33 -0400

    continued chat functionality

--- chat/index.php
+++ chat/index.php
@@ -43,7 +43,15 @@ else{
         <div style="clear:both"></div>
     </div>
 
-    <div id="chatbox"></div>
+      <div id="chatbox"><?php
+      if(file_exists("log.html") && filesize("log.html") > 0){
+      $handle = fopen("log.html", "r");
+      $contents = fread($handle, filesize("log.html"));
+      fclose($handle);
+
+      echo $contents;
+    }
+    ?></div>
 
     <form name="message" action="">
         <input name="usermsg" type="text" id="usermsg" size="63" />
@@ -56,6 +64,29 @@ else{
 $(document).ready(function(){
 
 });
+$("#submitmsg").click(function(){
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});
+		$("#usermsg").attr("value", "");
+		return false;
+	});
+  function loadLog(){
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div
+
+				//Auto-scroll
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}
+		  	},
+		});
+	}
+  setInterval (loadLog, 2500);
 </script>
 <?php
 }
