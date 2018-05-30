0221457affbc5a6a9e4d00fa9693b6ec0bc91108
Pablo Garza <pgarzabo2@hotmail.com>
2018-05-30 02:36:37 -0400

    new chat functionality

--- chat/index.php
+++ chat/index.php
@@ -1,109 +1,78 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?
-session_start();
 
-function loginForm(){
-    echo'
-    <div id="loginform">
-    <form action="index.php" method="post">
-        <p>Please enter your name to continue:</p>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" />
-        <input type="submit" name="enter" id="enter" value="Enter" />
+<div id="page-wrap">
+
+    <h2>jQuery/PHP Chat</h2>
+
+    <p id="name-area"></p>
+
+    <div id="chat-wrap"><div id="chat-area"></div></div>
+
+    <form id="send-message-area">
+        <p>Your message: </p>
+        <textarea id="sendie" maxlength = '100'></textarea>
     </form>
-    </div>
-    ';
-}
 
-if(isset($_POST['enter'])){
-    if($_POST['name'] != ""){
-        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
-    }
-    else{
-        echo '<span class="error">Please type in a name</span>';
-    }
-}
-if(isset($_GET['logout'])){
-
-    //Simple exit message
-    $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
-    fclose($fp);
-
-    session_destroy();
-    header("Location: index.php"); //Redirect the user
-}
-?>
-<?php
-if(!isset($_SESSION['name'])){
-    loginForm();
-}
-else{
-?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>Chat - Customer Module</title>
-<link type="text/css" rel="stylesheet" href="../css/style.css" />
-</head>
-
-<div id="wrapper">
-    <div id="menu">
-        <p class="welcome">Welcome, <b></b></p>
-        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-        <div style="clear:both"></div>
-    </div>
-
-      <div id="chatbox"><?php
-      if(file_exists("log.html") && filesize("log.html") > 0){
-      $handle = fopen("log.html", "r");
-      $contents = fread($handle, filesize("log.html"));
-      fclose($handle);
-
-      echo $contents;
-    }
-    ?></div>
-
-    <form name="message" action="">
-        <input name="usermsg" type="text" id="usermsg" size="63" />
-        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-    </form>
 </div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-<script type="text/javascript">
-// jQuery Document
-$(document).ready(function(){
-	//If user wants to end session
-	$("#exit").click(function(){
-		var exit = confirm("Are you sure you want to end the session?");
-		if(exit==true){window.location = 'index.php?logout=true';}
-	});
-});
-$("#submitmsg").click(function(){
-		var clientmsg = $("#usermsg").val();
-		$.post("post.php", {text: clientmsg});
-		$("#usermsg").attr("value", "");
-		return false;
-});
-  function loadLog(){
-		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-		$.ajax({
-			url: "log.html",
-			cache: false,
-			success: function(html){
-				$("#chatbox").html(html); //Insert chat log into the #chatbox div
+<body onload="setInterval('chat.update()', 1000)">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script src="chat.js"></script>
+<script>
 
-				//Auto-scroll
-				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-				if(newscrollHeight > oldscrollHeight){
-					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-				}
-		  	},
-		});
-	}
-  setInterval (loadLog, 2500);
+  // ask user for name with popup prompt
+  var name = prompt("Enter your chat name:", "Guest");
+
+  // default name is 'Guest'
+  if (!name || name === ' ') {
+    name = "Guest";
+  }
+
+  // strip tags
+  name = name.replace(/(<([^>]+)>)/ig,"");
+
+  // display name on page
+  $("#name-area").html("You are: <span>" + name + "</span>");
+
+  // kick off chat
+  var chat =  new Chat();
+
+  $(function() {
+
+     chat.getState();
+
+     // watch textarea for key presses
+     $("#sendie").keydown(function(event) {
+
+         var key = event.which;
+
+         //all keys including return.
+         if (key >= 33) {
+
+             var maxLength = $(this).attr("maxlength");
+             var length = this.value.length;
+
+             // don't allow new content if length is maxed out
+             if (length >= maxLength) {
+                 event.preventDefault();
+             }
+         }
+                                                                                                     });
+     // watch textarea for release of key press
+     $('#sendie').keyup(function(e) {
+
+        if (e.keyCode == 13) {
+
+              var text = $(this).val();
+              var maxLength = $(this).attr("maxlength");
+              var length = text.length;
+
+              // send
+              if (length <= maxLength + 1) {
+                chat.send(text, name);
+                $(this).val("");
+              } else {
+                $(this).val(text.substring(0, maxLength));
+              }
+        }
+     });
+  });
 </script>
-<?php
-}
-?>
-</body>
-</html>
