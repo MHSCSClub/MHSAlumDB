da5633e9d05bc0b0887d09c7197330bcbeb54317
mb10456 <maxdbobby@gmail.com>
2018-05-30 13:34:37 -0400

    new ui changes, new login ui changes

--- homepage/index.php
+++ homepage/index.php
@@ -172,7 +172,7 @@
 
         <div class="row featurette">
           <div class="col-md-7">
-            <h2 class="featurette-heading">Reconnect with old friends. <span class="text-muted">Profiles for almost every alumni.</span></h2>
+            <h4 class="featurette-heading">Reconnect with old friends. <span class="text-muted">Profiles for almost every alumni.</span></h4>
             <p class="lead">Open the directory and click on names to view profiles on every alumni. </p>
           </div>
           <div class="col-md-5">
@@ -184,7 +184,7 @@
 
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Check out the chat <span class="text-muted">See for yourself.</span></h2>
+            <h4 class="featurette-heading">Check out the chat. <span class="text-muted">See for yourself.</span></h4>
             <p class="lead">Click the chat link in the navbar</p>
           </div>
           <div class="col-md-5 order-md-1">
@@ -196,8 +196,8 @@
 
         <div class="row featurette">
           <div class="col-md-7">
-            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            <h4 class="featurette-heading">Sample news post. <span class="text-muted">Checkmate.</span></h4>
+            <p class="lead">Check out this new thing going on at MHS.</p>
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_163b16a3e66%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_163b16a3e66%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.13333129882812%22%20y%3D%22261.4%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
