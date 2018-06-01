059b6bb8af4e3c86146137499ce70d3b741aa12a
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-01 10:21:22 -0400

    chat login change 2

--- chat/inc/login.inc.php
+++ chat/inc/login.inc.php
@@ -55,6 +55,8 @@ class SimpleLoginSystem {
         unset($_COOKIE['member_pass']);
     }
     function check_login($sName, $sPass) {
+      include("../../php/signal.class.php");
+      include("../../php/auth.php");
         return auth::login($_POST['username'], $_POST['password']);
     }
 }
