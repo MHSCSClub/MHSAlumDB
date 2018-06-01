f911ad0619fcf0539f11031ad41751daa3eb0ea5
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-01 10:31:39 -0400

    include statements move

--- chat/inc/login.inc.php
+++ chat/inc/login.inc.php
@@ -1,4 +1,6 @@
-<?
+<?php
+include("../php/signal.class.php");
+include("../php/auth.php");
 // class SimpleLoginSystem
 class SimpleLoginSystem {
     // variables
@@ -55,8 +57,7 @@ class SimpleLoginSystem {
         unset($_COOKIE['member_pass']);
     }
     function check_login($sName, $sPass) {
-      include("../php/signal.class.php");
-      include("../php/auth.php");
+
         return auth::login($_POST['username'], $_POST['password']);
     }
 }
