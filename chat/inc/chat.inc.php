7403893da202afed18a918b0f10b7163ca6c24f3
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-06 15:31:45 -0400

    chat getmessages function change

--- chat/inc/chat.inc.php
+++ chat/inc/chat.inc.php
@@ -41,10 +41,9 @@ class SimpleChat {
         $sShoutboxForm = ob_get_clean();
         return $sShoutboxForm;
     }
-    function getMessages() {
+    function getMessages($user1, $user2) {
         $conn = new mysqli($this->sDbhost, $this->sDbUser, $this->sDbPass, $this->sDbName);
-        $user1 = 'pablogarza917@gmail.com';
-        $user2 = 'pgarzabo2@hotmail.com';
+
         //select the database
         //mysql_select_db($this->sDbName);
         $query = "SELECT * FROM `s_chat_messages` WHERE `user` = '$user1' OR `user` = '$user2' ORDER BY `when` ASC LIMIT 30";
