45f27094ff73d16faabc2dd67b8dfee392c89542
mb10456 <maxdbobby@gmail.com>
2018-06-06 11:04:11 -0400

    remove bad ui choices from buttons

--- chatv3/indivmessages.php
+++ chatv3/indivmessages.php
@@ -24,24 +24,54 @@
     ?>
     <!DOCTYPE html>
     <html lang="en">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <meta name="description" content=""></meta>
-        <meta name="author" content=""></meta>
-        <link rel="shortcut icon" href="/favicon.ico" />
-    
-        <title>Conversation</title>
-    
-        <!-- Bootstrap Core CSS -->
-        <link href="../css/bootstrap.css" rel="stylesheet"></link>
-    
-    </head>
-    
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <meta name="description" content=""></meta>
+            <meta name="author" content=""></meta>
+            <link rel="shortcut icon" href="/favicon.ico" />
+        
+            <title>Conversation</title>
+        
+            <!-- Bootstrap Core CSS -->
+            <link href="../css/bootstrap.css" rel="stylesheet"></link>
+        
+        </head>
         <body>
-            <a href="/chatv3/inbox.php" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true">My inbox</a>
-            <br>
-            <a href="/chatv3/sendmail.php" class="btn btn-primary btn-lg btn-block active" role="button" aria-pressed="true">My outbox</a>
-        </body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">MHS Alumni Database</a>
+            </div>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/homepage">Homepage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/mainprofile">My Profile</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Directories
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/ui">Main Directory</a>
+                            <a class="dropdown-item" href="/internshipui">Internship Directory</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/events">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/chat">Chat</a>
+                    </li>    
+                </ul>
+            </div>  
+        </nav>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="../resources/js/jquery-slim.min.js"><\/script>')</script>
+        <script src="../resources/js/popper.min.js"></script>
+        <script src="../resources/js/bootstrap.min.js"></script>
+    </body>
     </html>
     <?
     $chatid = $_GET["chatid"]; //get unique chat id from link
