548aa310da6e372ae7337af205436ecedd363541
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-07 12:46:45 -0400

    chat page test

--- chat/messages.php
+++ chat/messages.php
@@ -5,7 +5,6 @@ session_start();
 $oSimpleChat = new SimpleChat();
 $user1 = $oSimpleChat->currUser;
 $user2 = 'pgarzamw3@gmail.com';
-echo "Username: " . $user1;
 $oSimpleChat->check_auth();
 echo $oSimpleChat->getMessages($user1, $user2);
 ?>
