12724250c42cd04b44f1476103de090727b84341
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-08 11:25:45 -0400

    csv change 2

--- spreadsheetsphp/index.php
+++ spreadsheetsphp/index.php
@@ -1,14 +1,26 @@
 <?php
-header("Content-type: text/csv");
-		 header("Content-Disposition: attachment; filename=file.csv");
-		 $data = array(
-				 array("data12", "data16", "data17"),
-				 array("data2", "data33", "data25"),
-				 array("data31", "data32", "data23")
-		 );
-		 $file = fopen('php://output', 'w');
-		 fputcsv($file, array('Description', 'Click', 'CTR'));
-		 while ($data as $row) {
-			 fputcsv($file, $row);
-		 }
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+
+// fetch the data
+/*mysql_connect('localhost', 'username', 'password');
+mysql_select_db('database');
+$rows = mysql_query('SELECT field1,field2,field3 FROM table');
+
+// loop over the rows, outputting them
+while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);*/
+
+$data = array('hello', 'my', 'name','is','pablo');
+$counter = 0;
+while($counter < count($data)){
+	fputcsv($output, $data[$counter]);
+	$counter = $counter + 1;
+}
 ?>
