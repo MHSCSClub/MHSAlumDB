183abaaefae080579828045d86c49f147cab68c7
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-08 10:08:37 -0400

    xlsx update

--- spreadsheetsphp/index.php
+++ spreadsheetsphp/index.php
@@ -1,11 +1,11 @@
 <?php
-	header( "Content-Type: application/vnd.ms-excel" );
+	header( "Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" );
 	header( "Content-disposition: attachment; filename=spreadsheet.xls" );
-	
+
 	// print your data here. note the following:
 	// - cells/columns are separated by tabs ("\t")
 	// - rows are separated by newlines ("\n")
-	
+
 	// for example:
 	echo 'First Name' . "\t" . 'Last Name' . "\t" . 'Phone' . "\n";
 	echo 'John' . "\t" . 'Doe' . "\t" . '555-5555' . "\n";
