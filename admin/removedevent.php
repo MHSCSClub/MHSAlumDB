50853a73ecb86583897a0977d84e96279849acea
mb10456 <maxdbobby@gmail.com>
2018-06-08 11:56:05 -0400

    remove events

---
+++ admin/removedevent.php
@@ -0,0 +1,39 @@
+<?php
+    ini_set('display_errors', 1);
+    echo 'You have successfully logged into the admin page ';
+    include( '../php/rds.php' );
+    include("../php/signal.class.php");
+    include("../php/auth.php");
+    session_start();
+    if(!isset($_COOKIE['alumdbauth_admin'])){
+        echo "you do not have access to this page";
+        // sleep for 3 seconds
+        sleep(3);
+        header("location: /auth/");
+            exit;
+    } else {
+        $resp = auth::check_auth($_COOKIE['alumdbauth_admin']);
+        if($resp->isError()){
+            echo "you do not have access to this page";
+            // sleep for 3 seconds
+            sleep(3);
+            header("location: /auth/");
+            exit;
+        }
+
+    }
+    include ('../php/rds.php');
+    $conn = new mysqli($dbhost, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Welcome to the edit events page";
+
+    $id = $_GET["id"];
+
+    $stmt = $conn->prepare('DELETE FROM events WHERE id = ?');
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+    $stmt->close();
+
+?>
