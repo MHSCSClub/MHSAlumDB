19f2b13fc9b1b31ad530f40cf577e90fd78cab31
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-08 11:07:36 -0400

    csv instead of xlsx

--- spreadsheetsphp/index.php
+++ spreadsheetsphp/index.php
@@ -1,12 +1,21 @@
 <?php
-	header( "Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" );
-	header( "Content-disposition: attachment; filename=spreadsheet.xlsx" );
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
 
-	// print your data here. note the following:
-	// - cells/columns are separated by tabs ("\t")
-	// - rows are separated by newlines ("\n")
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
 
-	// for example:
-	echo 'First Name' . "\t" . 'Last Name' . "\t" . 'Phone' . "\n";
-	echo 'John' . "\t" . 'Doe' . "\t" . '555-5555' . "\n";
+// output the column headings
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+
+// fetch the data
+/*mysql_connect('localhost', 'username', 'password');
+mysql_select_db('database');
+$rows = mysql_query('SELECT field1,field2,field3 FROM table');
+
+// loop over the rows, outputting them
+while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);*/
+
+fputcsv($output, array('Hello', 'my', 'name', 'is', 'Pablo'));
 ?>
