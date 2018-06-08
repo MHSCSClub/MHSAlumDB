1e560189ed9d291761e3030e4972681b10521cb4
Pablo Garza <pgarzabo2@hotmail.com>
2018-06-08 09:59:45 -0400

    spreadsheet fix 1

--- chat/index.php
+++ chat/index.php
@@ -2,7 +2,7 @@
 <input type="button" value="Put Your Text Here" onclick="window.location.href='https://alumdb.mamaroneckschoolsfoundation.org/chat/convo.html'" />
 </form>
 <form>
-  <input type="button" value="Speadsheet" onclick="window.location.href='https://alumdb.mamaroneckschoolsfoundation.org/spreadsheet/index.php'"/>
+<input type="button" value="Speadsheet" onclick="window.location.href='https://alumdb.mamaroneckschoolsfoundation.org/spreadsheetsphp/index.php'"/>
 </form>
 <?
 ini_set('display_errors', 1);
