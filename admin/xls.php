c6066a9a3965ca15a15a3797ff819dcf6fa2ab19
mb10456 <maxdbobby@gmail.com>
2018-06-18 13:20:04 -0400

    fix up

--- admin/xls.php
+++ admin/xls.php
@@ -48,4 +48,10 @@
     }
     die;
 }
+
+function mysqli_field_name($result, $field_offset)
+{
+    $properties = mysqli_fetch_field_direct($result, $field_offset);
+    return is_object($properties) ? $properties->name : null;
+}
 ?>
