0a4b40d88cd8790a4af7536de943bd511f61c41c
mb10456 <maxdbobby@gmail.com>
2018-06-24 14:51:16 -0400

    fixes to html

--- admin/xls/index.php
+++ admin/xls/index.php
@@ -1,9 +1,9 @@
 <?php
     ini_set('display_errors', 1);
     echo 'You have successfully logged into the admin page ';
-    include( '../php/rds.php' );
-    include("../php/signal.class.php");
-    include("../php/auth.php");
+    include( '../../php/rds.php' );
+    include("../../php/signal.class.php");
+    include("../../php/auth.php");
     session_start();
     if(!isset($_COOKIE['alumdbauth_admin'])){
         echo "you do not have access to this page";
@@ -21,71 +21,41 @@
             exit;
         }
     }
-?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<title>MHS Alum DB - Log-in</title>
-		<link rel="shortcut icon" href="/favicon.ico" />
-		<!-- Bootstrap core CSS -->
-		<link href="../../css/bootstrap.min.css" rel="stylesheet">
-  	</head>
-
-  	<body>
-		<form method="post" class = "form-search">
-            <div class="form-group">
-                <label for="gyear">Spreadsheet by graduation year</label>
-                <input type="text" class="form-control" id="gyear" name = "gyear">
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </div>
-		</form>
-		
-	</body>
-</html>
-
-<?php
-    $conn = new mysqli($dbhost, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    echo "success1";
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $gyear = $_POST['gyear'];
+        echo "success";
+        include ('../php/rds.php');
+        $conn = new mysqli($dbhost, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $select = "";
+        $result = $conn->query("SELECT firstName, lastName, graduationYear FROM alum_info WHERE graduationYear = $gyear");
+        if (!$result) die('Couldn\'t fetch records');
+        $num_fields = mysqli_num_fields($result);
+        $headers = array();
+        for ($i = 0; $i < $num_fields; $i++) {
+            $headers[] = mysqli_field_name($result , $i);
+        }
+        $fp = fopen('php://output', 'w');
+        if ($fp && $result) {
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename="export.csv"');
+        header('Pragma: no-cache');
+        header('Expires: 0');
+        fputcsv($fp, $headers);
+        while ($row = $result->fetch_array(MYSQLI_NUM)) {
+            fputcsv($fp, array_values($row));
+        }
+        die;
         
-        
     }
-    echo "success";
-    include ('../php/rds.php');
-    $conn = new mysqli($dbhost, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    else{
+        echo "success 3";
+        readfile("csv.html");
     }
-    $select = "";
-    $result = $conn->query("SELECT firstName, lastName, graduationYear FROM alum_info WHERE graduationYear = $gyear");
-    if (!$result) die('Couldn\'t fetch records');
-    $num_fields = mysqli_num_fields($result);
-    $headers = array();
-    for ($i = 0; $i < $num_fields; $i++) {
-        $headers[] = mysqli_field_name($result , $i);
-    }
-    $fp = fopen('php://output', 'w');
-    if ($fp && $result) {
-    header('Content-Type: text/csv');
-    header('Content-Disposition: attachment; filename="export.csv"');
-    header('Pragma: no-cache');
-    header('Expires: 0');
-    fputcsv($fp, $headers);
-    while ($row = $result->fetch_array(MYSQLI_NUM)) {
-        fputcsv($fp, array_values($row));
-    }
-    die;
+    
 }
 function mysqli_field_name($result, $field_offset)
 {
